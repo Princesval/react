61c7cdab41ab222134448619f517ab688b05453e
João Vitor Princesval Caetano <163350089+Princesval@users.noreply.github.com>
2026-03-26 09:49:25 -0300

    color picker

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,9 +1,9 @@
-import MyComponent from "./MyComponent.jsx";
+import ColorPicker from "./ColorPicker/ColorPicker.jsx"; 
 
 function App() {
   return(
     <>
-      <MyComponent/>
+      <ColorPicker/>
     </>
    
   );
