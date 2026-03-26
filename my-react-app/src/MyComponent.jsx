af98c6cb881cf1ac20969014adc1d8a33d51e03c
João Vitor Princesval Caetano <163350089+Princesval@users.noreply.github.com>
2026-03-26 12:05:58 -0300

    updater functions

--- my-react-app/src/MyComponent.jsx
+++ my-react-app/src/MyComponent.jsx
@@ -29,9 +29,9 @@ function MyComponent(){
                 <p>Is employed? {isEmployed ? "Yes" : "No"}</p>
                 <button onClick={toggleStatus}>Toggle status</button>
             </div>
-    );
-    */
+    ); */
 
+    /* useState(onChange)
     const [name, setName] = useState(""); 
 
     function handleName(event) {
@@ -43,7 +43,36 @@ function MyComponent(){
             <input value={name} onChange={handleName}/>
             <p>Name: {name}</p>
         </div>
-    );
+    ); */
+
+    const [count, setCount] = useState(0);
+
+    const increment = () => { // Arrow function atuliza o valor atual de count em cada iteração 
+        setCount(c => c + 1);
+        setCount(c => c + 1);
+        setCount(c => c + 1);
+    }
+
+    const dencrement = () => { // Isso NÃO funciona porque o React contabiliza todos os count ainda como o valor original
+        setCount(count - 1);
+        setCount(count - 1);
+        setCount(count - 1);
+    }
+
+    const reset = () => {
+        setCount(0);
+    }
+
+    return(
+        <div>
+            <p>Counter: {count}</p>
+            <br />
+            <button onClick={increment}>+</button>
+            <button onClick={dencrement}>-</button>
+            <button onClick={reset}>Reset</button>
+        </div>
+
+    )
 }
 
 export default MyComponent
