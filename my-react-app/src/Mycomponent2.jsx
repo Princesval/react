932ec3b6178d48b174d763ae382e448adcf11811
João Vitor Princesval Caetano <163350089+Princesval@users.noreply.github.com>
2026-03-30 10:26:34 -0300

    uptade arrays

--- my-react-app/src/Mycomponent2.jsx
+++ my-react-app/src/Mycomponent2.jsx
@@ -1,7 +1,8 @@
 import React, {useState} from 'react';
 
-function MyComponent2 () {
+function MyComponent2() {
 
+    /* Update object
     const [car, setCar] = useState({year: 2024, 
                                     make: "Ford", 
                                     model: "Mustang"});
@@ -30,7 +31,30 @@ function MyComponent2 () {
             <input type="text" value={car.make} onChange={handleMakeChange}/> <br/>
             <input type="text" value={car.model} onChange={handleModelChange}/> <br/>
         </div>
+    ); */
+    const [foods, setFoods] = useState(["Apple", "Orange", "Banana"]);
+
+    function handleAddFood(){
+        const newFood = document.getElementById("foodInput").value;
+        document.getElementById("foodInput").value = "";
+
+        setFoods(f => [...f, newFood])
+    }
+    function handleRemoveFood(index){
+        setFoods(foods.filter((_, i) => i !== index))
+    }
+
+    return(
+        <div>
+            <h2>List of food</h2>
+            <ul>
+                {foods.map((food, index) => <li key={index} onClick={() => handleRemoveFood(index)}>{food}</li>)}
+            </ul>
+            <input type="text" id='foodInput' placeholder='Enter food name'/>
+            <button onClick={handleAddFood}>Add Food</button>
+        </div>
     );
+
 }
 
 export default MyComponent2
