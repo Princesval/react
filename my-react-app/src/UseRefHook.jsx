c5f5e2ce7a5232e2ca88e2b76e0d9fedefbe7fc8
João Vitor Princesval Caetano <163350089+Princesval@users.noreply.github.com>
2026-04-08 14:43:45 -0300

    useRef comentário

--- my-react-app/src/UseRefHook.jsx
+++ my-react-app/src/UseRefHook.jsx
@@ -1,5 +1,10 @@
 import React, {useState, useEffect, useRef} from "react";
 
+/* useRef não e-renderiza quando um valor muda
+1. Acessando e interagindo com elementos do DOM
+2. Lidando com Focus, Animations e Transitions
+3. Lidanod com Timers e Intervals
+*/
 function UseRefHook() {
 
     const ref = useRef(0);
