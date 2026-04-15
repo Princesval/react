e7f64662f22887d8031a1d339be2b4145b28e9b8
João Vitor Princesval Caetano <163350089+Princesval@users.noreply.github.com>
2026-04-15 10:59:47 -0300

    props definição

--- my-react-app/src/Student.jsx
+++ my-react-app/src/Student.jsx
@@ -9,3 +9,7 @@ function Student(props) {
 }
 
 export default Student
+
+/* 
+Props: dados passados de um componente pai para um componente filho. Elas permitem que os componentes sejam reutilizáveis e dinâmicos, já que você pode mudar o conteúdo ou comportamento deles sem alterar o componente em si.
+*/
